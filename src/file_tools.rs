8dda454d51362b262afc221d3e2ae54ffca8a805
infographtheory <contact@infotriple.com>
2025-02-16 16:38:48 +0100

    clean up and document file_tools

--- src/file_tools.rs
+++ src/file_tools.rs
@@ -3,20 +3,18 @@ use std::fs;
 
 use crate::list_tools;
 
+///
+/// Appeds the argument line to the file file_name 
+///
 pub fn append_ln_to_file(line: &str, file_name: &str) {
-//println!("Filename:{:?}",file_name);
-//println!("line:{:?}",line);
     let lines = fs::read_to_string(file_name).unwrap();
     let text = list_tools::append_ln(line, &lines);
     let _ = fs::write(file_name, text);
 }
 
-pub fn sort_and_append_ln_to_file(line: &str, file_name: &str) {
-     let lines = fs::read_to_string(file_name).unwrap();
-    let text = list_tools::append_ln(line, &lines);
-    let _ = fs::write(file_name, text);
-}
-
+///
+/// Write argument text to file in argument file_name.
+///
 pub fn write(file_name: String, text: &str) {
     let _ = fs::write(file_name.as_str(), text);
 }
