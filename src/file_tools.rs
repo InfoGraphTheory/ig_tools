e451b46e030d246b4ade93ff27d0e85be9fa91d2
infographtheory <contact@infotriple.com>
2025-03-11 09:59:24 +0100

    use PathBuf

--- src/file_tools.rs
+++ src/file_tools.rs
@@ -1,22 +1,22 @@
 
-use std::fs;
+use std::{fs, path::PathBuf};
 
 use crate::list_tools;
 
 ///
 /// Appeds the argument line to the file file_name 
 ///
-pub fn append_ln_to_file(line: &str, file_name: &str) {
-    let lines = fs::read_to_string(file_name).unwrap();
+pub fn append_ln_to_file(line: &str, file_name: PathBuf) {
+    let lines = fs::read_to_string(file_name.clone()).unwrap();
     let text = list_tools::append_ln(line, &lines);
-    let _ = fs::write(file_name, text);
+    let _ = fs::write(file_name.clone(), text);
 }
 
 ///
 /// Write argument text to file in argument file_name.
 ///
-pub fn write(file_name: String, text: &str) {
-    let _ = fs::write(file_name.as_str(), text);
+pub fn write(file_name: PathBuf, text: &str) {
+    let _ = fs::write(file_name.clone(), text);
 }
 
 
